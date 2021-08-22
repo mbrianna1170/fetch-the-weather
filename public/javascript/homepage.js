e736ea5dccaaa980fd01766988a1ff56767b7c87
mbrianna1170 <brianna.miller09@yahoo.com>
2021-08-21 23:29:10 -0500

    api working

---
+++ public/javascript/homepage.js
@@ -0,0 +1,8 @@
+
+
+const getWeather = function() {
+    // const response = fetch("https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=dd12231eebcfc81237b12aa64539d58d");
+    console.log(response);
+};
+
+getWeather();

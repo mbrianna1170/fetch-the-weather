5f32663e5bf85354701f889f899411e9985c3bba
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-19 21:56:40 -0500

    fixed sumbithandler function

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
-var getWeather = function(city) {
+// variables
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#city");
+
+var getWeather = function (city) {
   // format api url
   var apiUrl = "https://api.openweathermap.org/data/2.5/find?q=" + city + "&units=imperial&appid=dd12231eebcfc81237b12aa64539d58d";
 
   // fetch api
-  fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
       console.log(data);
     });
   });
 };
 
-getWeather("Austin");
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  // get value from input element
+var city = cityInputEl.value.trim();
+
+if (city) {
+  getWeather(city);
+  cityInputEl.value = "";
+} else {
+  alert("Please enter the name of a city.");
+}
+  console.log(event);
+};
+
+cityFormEl.addEventListener("submit", formSubmitHandler);

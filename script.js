46e42932c3de6a9b0bfe5dda886c48cb652ad8bc
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-20 01:20:49 -0500

    moment js working

--- script.js
+++ script.js
@@ -7,12 +7,15 @@ var cityName = document.querySelector("#city-name");
 var cityTemperature = document.querySelector("#temperature");
 var cityWind = document.querySelector("#wind");
 var cityHumidity = document.querySelector("#humidity");
-
+var currentDate = document.querySelector("#current-date");
 
 // Fetch Weather Information
 var getWeather = function (city) {
   // format api url
-  var apiUrl = "https://api.openweathermap.org/data/2.5/find?q=" + city + "&units=imperial&appid=dd12231eebcfc81237b12aa64539d58d";
+  var apiUrl =
+    "https://api.openweathermap.org/data/2.5/find?q=" +
+    city +
+    "&units=imperial&appid=dd12231eebcfc81237b12aa64539d58d";
 
   // fetch api
   fetch(apiUrl).then(function (response) {
@@ -37,15 +40,16 @@ var formSubmitHandler = function (event) {
   console.log(event);
 };
 
-// Display Weather On Page
+// Display Current Weather On Page
 var displayWeather = function (data) {
-  console.log(data)
-  // clear old content
-  // weatherContainerEl.textContent = "";
+  console.log(data);
   cityName.textContent = data.list[0].name;
   cityTemperature.textContent = data.list[0].main.temp + "\xB0" + "F";
   cityWind.textContent = data.list[0].wind.speed + " MPH";
   cityHumidity.textContent = data.list[0].main.humidity + "%";
-}
+  // display date
+  var today = moment().format("MM/DD/YY");
+  currentDate.textContent = today;
+};
 
 searchformEl.addEventListener("submit", formSubmitHandler);

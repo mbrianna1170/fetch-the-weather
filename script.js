4ef8428774fb2b2b080126b30e5280332b75cfab
mbrianna1170 <brianna.miller09@yahoo.com>
2021-10-19 20:32:57 -0500

    fetch api is working

--- script.js
+++ script.js
@@ -1,46 +1,13 @@
-// API variables
-const apiURL = 'pro.openweathermap.org';
-const apiKey = "dd12231eebcfc81237b12aa64539d58d";
-// DOM elements
-let searchFormEl = document.querySelector("#search-form");
-let cityInputEl = document.querySelector("#city-input");
-let cityNameEl = document.querySelector("#city-name");
-let cityName = cityNameEl;
-// Other
-let searchHistoryArray = [];
+var getWeather = function(city) {
+  // format api url
+  var apiUrl = "https://api.openweathermap.org/data/2.5/find?q=" + city + "&units=imperial&appid=dd12231eebcfc81237b12aa64539d58d";
 
-// fetch api getWeather(); 
-const getWeather = function (city) {
-  fetch(`${apiURL}/data/2.5/forecast/hourly?q=${cityName}n&appid=${apiKey}`)
-    .then(function (response) {
-      if (response.ok) {
-        response.json().then(function (data) {
-          displayWeather(data, city);
-        });
-      } else {
-        alert("Error: City Not Found")
-      }
+  // fetch api
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
     });
+  });
 };
 
-// displayWeather();
-const displayWeather = function (data) {
-  cityNameEl.textContent = "Showing weather for " + data.list[0].name;
-}
-
-// Form Sumbit Handler
-const formSumbitHandler = function (event) {
-  event.preventDefault();
-  const cityName = cityInputEl.value.trim();
-  if (cityName) {
-    getWeather(cityName);
-    searchHistoryArray.unshift(cityName);
-    localStorage.setItem("city", JSON.stringify(searchHistoryArray));
-    createButton();
-    cityInputEl.value = "";
-  } else {
-    alert("Please enter a valid city.");
-  }
-};
-
-searchFormEl.addEventListener("sumbit", formSumbitHandler);
+getWeather("Austin");
